f62d0317871feec829eb8a9b5ca36e28d35bb009
Cedric Franz M Candido <cedricfranzmcandido@Cedrics-Mac-mini.local>
2024-07-08 15:05:09 +0800

    Add client order/ card view

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,11 +1,12 @@
 import { SignedIn, UserButton } from "@clerk/nextjs";
+import HeaderClient from "./headerCient";
+import Product from "./product";
 
 const Client = () => {
   return (
     <div>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      <HeaderClient />
+      <Product />
     </div>
   );
 };
